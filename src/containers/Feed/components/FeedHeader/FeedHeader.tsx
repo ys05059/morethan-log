348ff6c397799c1bc812ecf580b7caddda22bf67
ys05059 <ys05059@gmail.com>
2023-04-08 18:22:40 +0900

    fix : bug category not show on mobile

--- src/containers/Feed/components/FeedHeader/FeedHeader.tsx
+++ src/containers/Feed/components/FeedHeader/FeedHeader.tsx
@@ -9,11 +9,12 @@ type Props = {
 
 const FeedHeader: React.FC<Props> = ({ categories }) => {
   return (
-    <div className="flex border-b border-gray-300 mb-4 justify-between items-center ">
-      <CategorySelect data={categories} />
+    <div className="flex border-b border-gray-300 mb-4 justify-between md:justify-end items-center ">
+      <div className="block md:hidden">
+        <CategorySelect data={categories} />
+      </div>
       <OrderButtons />
     </div>
   )
-}
 
 export default FeedHeader
