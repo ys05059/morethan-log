d1d75e40b5bd05a6cff7b5f850cfaa1a667a2d8e
ys05059 <ys05059@gmail.com>
2023-04-08 17:19:09 +0900

    프로필 수정
    - 프로필 수정

--- site.config.js
+++ site.config.js
@@ -1,13 +1,13 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    name: "woongyee",
+    image: "/morethan-avartar_woongyee.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    role: "android developer",
+    bio: "[ENFP] I enjoy learning new things and communicating with others",
+    email: "ys05059@gmail.com",
+    linkedin: "",
+    github: "ys05059",
     instagram: "",
   },
   projects: [
@@ -24,12 +24,12 @@ const CONFIG = {
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
+  link: "https://woongyee-devlog.vercel.app",
+  since: 2023, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
   seo: {
-    keywords: ["Blog", "Website", "Notion"],
+    keywords: ["Blog", "Website", "Notion","morethanlog"],
   },
 
   // notion configuration (required)
